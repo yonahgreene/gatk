707baea0cd4cbf18d0dc0ab313e94fe0ebb593d5
Samuel Lee <lee.samuel.k@gmail.com>
2019-02-28 08:10:30 -0500

    minor typos

--- src/main/java/org/broadinstitute/hellbender/tools/copynumber/formats/collections/AbstractRecordCollection.java
+++ src/main/java/org/broadinstitute/hellbender/tools/copynumber/formats/collections/AbstractRecordCollection.java
@@ -19,7 +19,6 @@ import java.io.*;
 import java.util.List;
 import java.util.function.BiConsumer;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 
 /**
  * Represents {@link METADATA} (which can be represented as a {@link SAMFileHeader}),
@@ -81,7 +80,7 @@ public abstract class AbstractRecordCollection<METADATA extends Metadata, RECORD
         try (final RecordCollectionReader reader = new RecordCollectionReader(IOUtils.fileToPath(inputFile))) {
             metadata = MetadataUtils.fromHeader(reader.getHeader(), getMetadataType());
             TableUtils.checkMandatoryColumns(reader.columns(), mandatoryColumns, UserException.BadInput::new);
-            records = reader.stream().collect(Collectors.collectingAndThen(Collectors.toList(), ImmutableList::copyOf));
+            records = ImmutableList.copyOf(reader.stream().iterator());     //avoid creation of an intermediate list
         } catch (final IOException | UncheckedIOException e) {
             throw new UserException.CouldNotReadInputFile(inputFile, e);
         }
@@ -118,8 +117,8 @@ public abstract class AbstractRecordCollection<METADATA extends Metadata, RECORD
         } catch (final IOException e) {
             throw new UserException.CouldNotCreateOutputFile(outputFile, e);
         }
-        try (final RecordWriter recordWriter = new RecordWriter(new FileWriter(outputFile, true))) {
-            recordWriter.writeAllRecords(records);
+        try (final RecordCollectionWriter writer = new RecordCollectionWriter(new FileWriter(outputFile, true))) {
+            writer.writeAllRecords(records);
         } catch (final IOException e) {
             throw new UserException.CouldNotCreateOutputFile(outputFile, e);
         }
@@ -191,8 +190,8 @@ public abstract class AbstractRecordCollection<METADATA extends Metadata, RECORD
         }
     }
 
-    final class RecordWriter extends TableWriter<RECORD> {
-        RecordWriter(final Writer writer) throws IOException {
+    final class RecordCollectionWriter extends TableWriter<RECORD> {
+        RecordCollectionWriter(final Writer writer) throws IOException {
             super(writer, mandatoryColumns);
         }
 
